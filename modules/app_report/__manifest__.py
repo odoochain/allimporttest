52fd3204b7ca1be0972605178553e83f608f6107
jwambeek <70319876+jwambeek@users.noreply.github.com>
2020-11-24 16:07:40 +0530

    update

---
+++ modules/app_report/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name' : 'Tax Reports',
+    'version': '1.0',
+    'Summary': 'Tax Report Prints',
+    'description': 'To print the new report',
+    'license': 'LGPL-3',
+    'depends': [
+        'sale_management'
+    ],    
+    'data': [
+        'reports/report.xml',
+        'reports/tax_invoice.xml'
+    ],
+    'installable': True,
+    'application':True,
+    'auto_install':False
+}
