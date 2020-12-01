721727182bbcdbc02be7f22e4209c2f1ba1feb15
jwambeek <70319876+jwambeek@users.noreply.github.com>
2020-12-01 19:26:48 +0530

    update

---
+++ modules/app_report/models/app_report.py
@@ -0,0 +1,10 @@
+from odoo import models, fields, api
+
+class SaleOrder_Data(models.Model):
+    _inhert = 'sale.order'
+    order_ref = fields.Float(string = 'Sales Order Ref')
+
+class AccountMove_Data(models.Model):
+    _inherit = 'account.move'
+    salesorder_id = fields.Many2one('sale.order', string='Sales Order Id')
+    sale_order_ref = fields.Float(related = 'salesorder_id.order_ref', store = True)
