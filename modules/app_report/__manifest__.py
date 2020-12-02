d9a6a27d05b83b830ad6e1701c66f0fa97b8f442
jwambeek <70319876+jwambeek@users.noreply.github.com>
2020-12-02 07:48:53 +0530

    update

--- modules/app_report/__manifest__.py
+++ modules/app_report/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name' : 'Tax Reports',
-    'version': '1.2',
+    'version': '1.3',
     'Summary': 'Tax Report Prints',
     'description': 'To print the new report',
     'license': 'LGPL-3',
