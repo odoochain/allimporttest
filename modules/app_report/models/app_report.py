cea900a0088dfdbbfbf44915af39438e9d3cd0a7
jwambeek <70319876+jwambeek@users.noreply.github.com>
2020-12-02 07:54:07 +0530

    commented code

--- modules/app_report/models/app_report.py
+++ modules/app_report/models/app_report.py
@@ -10,13 +10,13 @@ class AccountMove_Data(models.Model):
     sale_order_ref = fields.Float(related = 'salesorder_id.order_ref', store = True)
     line_items_id = fields.One2many('account.move.line', 'move_id', string='Invoice Items')
 
-    @api.depends('line_items_id.price_unit', 'line_items_id.discount')
-    def _cal_total_discount(self):
-        for order in self:
-            cal_discount = 0
-            for line_items in order.line_items_id:
-                cal_discount = cal_discount + (line_items.price_unit * line_items.discount) / 100
-            order.calculated_discount = cal_discount
+    # @api.depends('line_items_id.price_unit', 'line_items_id.discount')
+    # def _cal_total_discount(self):
+    #     for order in self:
+    #         cal_discount = 0
+    #         for line_items in order.line_items_id:
+    #             cal_discount = cal_discount + (line_items.price_unit * line_items.discount) / 100
+    #         order.calculated_discount = cal_discount
         
 
-    calculated_discount = fields.Float(string = 'Discount', compute = '_cal_total_discount', store = True)
+    # calculated_discount = fields.Float(string = 'Discount', compute = '_cal_total_discount', store = True)
