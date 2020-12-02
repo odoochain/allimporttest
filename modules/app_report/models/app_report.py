552bd73d347f5d82fcc1d7ed4a1f51b24986ca2b
jwambeek <70319876+jwambeek@users.noreply.github.com>
2020-12-02 12:09:51 +0530

    update

--- modules/app_report/models/app_report.py
+++ modules/app_report/models/app_report.py
@@ -12,9 +12,6 @@ class AccountMove_Data(models.Model):
     _inherit = 'account.move'
     salesorder_id = fields.Many2one('sale.order', string='Sales Order Id')
     sale_order_ref = fields.Float(related = 'salesorder_id.order_ref', store = True)
-    # line_items_id = fields.One2many('account.move.line', 'move_id', string='Invoice Items Line')
-
-    # line_items_ids = fields.One2many('account.move.line', 'move_line_id', string='Invoice Items')
 
     @api.depends('line_ids.price_unit', 'line_ids.discount','line_ids.quantity')
     def _cal_total_discount(self):
@@ -29,6 +26,28 @@ class AccountMove_Data(models.Model):
         
 
     calculated_discount = fields.Float(string = 'Discount', compute = '_cal_total_discount', store = True)
+
+    @api.depends('calculated_discount', 'amount_total')
+    def _cal_total_baht_escl_vat(self):
+        self.total_baht_excl_VAT = self.amount_total - self.calculated_discount
+
+    total_baht_excl_VAT = fields.Float(string = 'Total Baht Excl VAT', compute = '_cal_total_baht_escl_vat', store= True)
+
+    @api.depends('total_baht_excl_VAT')
+    def _cal_total_baht_incl_vat(self):
+        self.total_baht_incl_VAT = self.total_baht_excl_VAT / 1.07
+
+    total_baht_incl_VAT = fields.Float(string = 'Total Baht Incl VAT', compute = '_cal_total_baht_incl_vat', store = True)
+
+    @api.depends('total_baht_excl_VAT','total_baht_incl_VAT')
+    def _cal_total_vat(self):
+        self.vat = self.total_baht_incl_VAT - self.total_baht_excl_VAT
+
+    vat = fields.Float(string = 'Vat', compute = '_cal_total_vat', store = True)
+
+    
+
+
 
 class AccountMove_Line_Data(models.Model):
     _inherit = 'account.move.line'
