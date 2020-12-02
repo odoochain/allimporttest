effde4feb32d7e2b850a5fde59fe29f0d29bcbd6
jwambeek <70319876+jwambeek@users.noreply.github.com>
2020-12-02 07:37:41 +0530

    update

--- modules/app_report/models/app_report.py
+++ modules/app_report/models/app_report.py
@@ -8,12 +8,13 @@ class AccountMove_Data(models.Model):
     _inherit = 'account.move'
     salesorder_id = fields.Many2one('sale.order', string='Sales Order Id')
     sale_order_ref = fields.Float(related = 'salesorder_id.order_ref', store = True)
+    line_items_id = fields.One2many('account.move.line', 'move_order_id', string='Invoice Items')
 
-    @api.depends('line_ids')
+    @api.depends('line_items_id.price_unit', 'line_items_id.discount')
     def _cal_total_discount(self):
         for order in self:
             cal_discount = 0
-            for line_items in self:
+            for line_items in order.line_items_id:
                 cal_discount = cal_discount + (line_items.price_unit * line_items.discount) / 100
             order.calculated_discount = cal_discount
         
