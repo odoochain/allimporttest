00a4e8dcabf31998b0f83e6055253b79effae8aa
jwambeek <70319876+jwambeek@users.noreply.github.com>
2020-12-02 12:55:28 +0530

    update

--- modules/app_report/models/app_report.py
+++ modules/app_report/models/app_report.py
@@ -25,28 +25,38 @@ class AccountMove_Data(models.Model):
             order.calculated_discount = cal_discount
         
 
-    calculated_discount = fields.Float(string = 'Discount', compute = '_cal_total_discount', store = True)
+    calculated_discount = fields.Float(string = 'Discount', compute = '_cal_total_discount', store = True, digits=(12,4))
 
-    @api.depends('calculated_discount', 'amount_total')
+    @api.depends('line_ids.price_unit','line_ids.quantity')
+    def _cal_total_price(self):
+        for orders in self:
+            order_total = 0
+            for lines in orders.line_ids:
+                order_total = order_total + (lines.price_unit * lines.quantity)
+            orders.price_totals = order_total
+
+    price_totals = fields.Float(string='Total Line Price', compute = '_cal_total_price',store = True, digits=(12,4))
+
+    @api.depends('calculated_discount', 'price_totals')
     def _cal_total_baht_escl_vat(self):
         for orders in self:
-            orders.total_baht_excl_VAT = orders.amount_total - orders.calculated_discount
+            orders.total_baht_excl_VAT = orders.price_totals - orders.calculated_discount
 
-    total_baht_excl_VAT = fields.Float(string = 'Total Baht Excl VAT', compute = '_cal_total_baht_escl_vat', store= True)
+    total_baht_excl_VAT = fields.Float(string = 'Total Baht Excl VAT', compute = '_cal_total_baht_escl_vat', store= True, digits=(12,4))
 
     @api.depends('total_baht_excl_VAT')
     def _cal_total_baht_incl_vat(self):
         for orders in self:
             orders.total_baht_incl_VAT = orders.total_baht_excl_VAT / 1.07
 
-    total_baht_incl_VAT = fields.Float(string = 'Total Baht Incl VAT', compute = '_cal_total_baht_incl_vat', store = True)
+    total_baht_incl_VAT = fields.Float(string = 'Total Baht Incl VAT', compute = '_cal_total_baht_incl_vat', store = True, digits=(12,4))
 
     @api.depends('total_baht_excl_VAT','total_baht_incl_VAT')
     def _cal_total_vat(self):
         for orders in self:
             orders.vat = orders.total_baht_incl_VAT - orders.total_baht_excl_VAT
 
-    vat = fields.Float(string = 'Vat', compute = '_cal_total_vat', store = True)
+    vat = fields.Float(string = 'Vat', compute = '_cal_total_vat', store = True, digits=(12,4))
 
     
 
