5555a3b143cdea4be799c4d3ccc6ec119a9e5cd7
Ranjeet Kumar <ranjeet.kumar@vemo.com>
2020-12-03 17:22:48 +0530

    update

--- modules/app_report/__manifest__.py
+++ modules/app_report/__manifest__.py
@@ -5,7 +5,7 @@
     'description': 'To print the new report',
     'license': 'LGPL-3',
     'depends': [
-        'sale_management','website','account_accountant'
+        'sale_management','website','account_accountant','report'
     ],    
     'data': [
         #'data/data.xml',
