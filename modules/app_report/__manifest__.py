3b464112ac6d129794d855ae13378646c597826a
jwambeek <70319876+jwambeek@users.noreply.github.com>
2020-12-03 11:01:26 +0530

    update

--- modules/app_report/__manifest__.py
+++ modules/app_report/__manifest__.py
@@ -8,10 +8,9 @@
         'sale_management','website','account_accountant'
     ],    
     'data': [
-        #'data/data.xml',
         'reports/report_tax_invoice.xml',
-        'reports/reports.xml',
-        'views/app_report_view.xml'
+        'reports/reports.xml'
+        # 'views/app_report_view.xml'
     ],
     'installable': True,
     'application':True,
--- modules/app_report/models/app_report.py
+++ modules/app_report/models/app_report.py
@@ -1,29 +1,15 @@
 from odoo import models, fields, api
-import logging
 
-
-_logger = logging.getLogger(__name__)
-
-class SaleOrder_Data(models.Model):
-    _inherit = 'sale.order'
-    channel_order_number = fields.Char(string = 'Sales Order Ref', store = True)
 
 class AccountMove_Data(models.Model):
     _inherit = 'account.move'
-    salesorder_id = fields.Many2one('sale.order', string='Sales Order Id', store = True)
-    sale_order_ref = fields.Char(related = 'salesorder_id.channel_order_number',string = 'Sales Order Reference', store = True)
-    sales_order_name = fields.Char(related = 'salesorder_id.name', string = 'Sales Order Name', store = True)
-
-
+    
     @api.depends('line_ids.price_unit', 'line_ids.discount','line_ids.quantity')
     def _cal_total_discount(self):
         for order in self:
             cal_discount = 0
             for line_items in order.line_ids:
                 cal_discount = cal_discount + (line_items.quantity * line_items.price_unit * line_items.discount) / 100
-            # _logger.warning('*************************************')
-            # _logger.warning("IT IS warn")
-            # _logger.warning(cal_discount)
             order.calculated_discount = cal_discount
         
 
