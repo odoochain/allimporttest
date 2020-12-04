670625396be3a5c5867728afff3e690317e45972
jwambeek <70319876+jwambeek@users.noreply.github.com>
2020-12-04 20:07:09 +0530

    update field

--- modules/app_report/__manifest__.py
+++ modules/app_report/__manifest__.py
@@ -9,7 +9,7 @@
     ],    
     'data': [
         'reports/report_tax_invoice.xml',
-        'reports/reports.xml'
+        'reports/reports.xml',
         'views/app_report_view.xml'
     ],
     'installable': True,
