703822b4cf2366f8ef397426cd21cea948785f29
Abhay shukla <33155803+rudra-shukla@users.noreply.github.com>
2021-02-04 14:55:02 +0530

    Update app_report.py

--- app_report/models/app_report.py
+++ app_report/models/app_report.py
@@ -6,7 +6,7 @@ class SaleOrder_Data(models.Model):
     _inherit = 'sale.order'
 
     channel_order_number = fields.Char(string = 'Channel Order')
-    channel_order_number1 = fields.Char(string = 'Channel Order1')
+    payment_type = fields.Char(string = 'Payment Type')
     
     
 
