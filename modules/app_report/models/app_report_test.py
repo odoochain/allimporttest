82a7e5b49f67a494b4ddfbec4386520f26850dc2
rani700 <shwetarani700@gmail.com>
2021-02-04 13:45:01 +0530

    Update app_report_test.py

--- modules/app_report/models/app_report_test.py
+++ modules/app_report/models/app_report_test.py
@@ -25,7 +25,7 @@ class SaleOrder_Data(models.Model):
             'source_id': self.source_id.id,
             'invoice_user_id': self.user_id and self.user_id.id,
             'team_id': self.team_id.id,
-            'channel_order_number':self.channel_order_number,
+            #'channel_order_number':self.channel_order_number,
             'partner_id': self.partner_invoice_id.id,
             'partner_shipping_id': self.partner_shipping_id.id,
             'fiscal_position_id': (self.fiscal_position_id or self.fiscal_position_id.get_fiscal_position(self.partner_invoice_id.id)).id,
@@ -79,7 +79,7 @@ class AccountMove_Data(models.Model):
 
     # seller_discount = fields.Float(string = 'Seller Discount',readonly=True, tracking=True)
 
-    channel_order_number = fields.Char(string = 'Channel Order No.',readonly=True, tracking=True)
+    #channel_order_number = fields.Char(string = 'Channel Order No.',readonly=True, tracking=True)
     #address_local_lang =  fields.Text(string = 'Address (Thai)', tracking=True,readonly=True)
     
     @api.depends('line_ids.price_unit', 'line_ids.seller_discount','line_ids.quantity')
