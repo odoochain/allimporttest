d82d588cbb6de7239ac645496892aebfc23b0278
Abhay shukla <33155803+rudra-shukla@users.noreply.github.com>
2021-02-12 14:51:18 +0530

    Create __manifest__.py

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name' : 'Hospital Management',
+    'version': '1.3',
+    'Summary': 'to test how to create a module',
+    'sequence': '10',
+    'category': 'Extra Tools',
+    'description': 'First module is created',
+    'license': 'LGPL-3',
+    'depends': [
+        'sale_management','website','account_accountant'
+    ],    
+    'data': [
+       #all the path of XML files are present here
+    ],
+    'installable': True,
+    'application':True,
+    'auto_install':False
+}
