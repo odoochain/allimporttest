3d6abf462c428b5d065d59cac3238d259fda8127
Abhay shukla <33155803+rudra-shukla@users.noreply.github.com>
2021-02-12 16:53:31 +0530

    Update patient.py

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -4,3 +4,9 @@ from odoo import models, fields, api
 
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
+    _description = 'Patient Record'
+    patient_name = fields.Char(string='Name')
+    patient_age = fields.Integer(string='Age')
+    Notes = fields.Text(string='Notes')
+    patient_image = fields.Binary(string='Image')
+    
